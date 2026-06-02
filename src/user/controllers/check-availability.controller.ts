7c1fb01dc57321ac3376ebad5a82cb575d13ea42
Nicolás Romani <nicolasromani.dev@gmail.com>
2026-06-02 10:50:46 -0300

    feat(user): add email validations and DTO to check-availability endpoint

--- src/user/controllers/check-availability.controller.ts
+++ src/user/controllers/check-availability.controller.ts
@@ -2,25 +2,37 @@ import type { NextFunction, Request, Response } from 'express'
 import type { ParamsDictionary } from 'express-serve-static-core'
 
 import { ExceptionFactory } from '../../shared/response/http/ExceptionFactory'
+import UserModel from '../models/user.model.js'
 import UserService from '../services/user.service'
 
-interface QueryBody {
+interface DataDTO {
    field?: 'username' | 'email'
    value: string
 }
 
-interface ResponseBody {
+class ResponseDTO {
    isAvailable: boolean
    message: string
-   data: {
-      field: QueryBody['field']
-      value: QueryBody['value']
+   data: DataDTO
+
+   constructor({ isAvailable, message, data }: { isAvailable: boolean, message: string, data: DataDTO }) {
+      this.isAvailable = isAvailable
+      this.message = message
+      this.data = data
+   }
+
+   public toJSON() {
+      return  {
+         isAvailable: this.isAvailable,
+         message: this.message,
+         data: this.data
+      }
    }
 }
 
 export default class CheckAvailabilityController {
-   private readonly req: Request<ParamsDictionary, any, QueryBody, any>
-   private readonly res: Response<ResponseBody, Record<string, any>>
+   private readonly req: Request<ParamsDictionary, any, DataDTO, any>
+   private readonly res: Response
    private readonly next: NextFunction
    private readonly userService = new UserService()
 
@@ -36,13 +48,31 @@ export default class CheckAvailabilityController {
       const { field, value } = this.req.body
       let isAvailable = true
 
-      if(this.req.headers['content-type'] !== 'application/json') {
+      if (this.req.headers['content-type'] !== 'application/json') {
          const exception = ExceptionFactory.contentTypeNotSupport('expected application/json')
          return this.next(exception)
       }
 
       if (!field || !value) return this.next(ExceptionFactory.invalidInput('field and value are required'))
       if (!['username', 'email'].includes(field)) return this.next(ExceptionFactory.invalidInput('field must be username or email'))
+
+      if (field === 'email' && value.length > UserModel.schema['tree'].email.maxlength[0]) {
+         const response = new ResponseDTO({
+            isAvailable: false,
+            message: 'Email is too long',
+            data: { field, value }
+         })
+         return this.res.status(200).json(response.toJSON())
+      }
+
+      if (field === 'email' && !UserModel.schema['tree'].email.match[0].test(value)) {
+         const response = new ResponseDTO({
+            isAvailable: false,
+            message: 'Invalid email format',
+            data: { field, value }
+         })
+         return this.res.status(200).json(response.toJSON())
+      }
 
       try {
          if (field === 'username') {
@@ -60,18 +90,11 @@ export default class CheckAvailabilityController {
 
       const capitalized = field.split('').map((char, index) => index === 0 ? char.toUpperCase() : char).join('')
 
-      if (!isAvailable) {
-         return this.res.status(200).json({
-            isAvailable,
-            message: capitalized + ' is already taken',
-            data: { field, value }
-         })
-      }
-
-      return this.res.status(200).json({
+      const response = new ResponseDTO({
          isAvailable,
-         message: capitalized + ' is available',
+         message: capitalized + (isAvailable ? ' is available' : ' is already taken'),
          data: { field, value }
       })
+      return this.res.status(200).json(response.toJSON())
    }
 }
